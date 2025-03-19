10b7a2e310d814c70f2df4268997e88adb593b1c
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-03-19 18:39:31 +0200

    Create EnumChatFormatting.cs

---
+++ net/minecraft/util/EnumChatFormatting.cs
@@ -0,0 +1,27 @@
+﻿namespace Minecraft1_8_9Port.net.minecraft.util;
+
+public enum EnumChatFormatting
+{
+    BLACK,
+    DARK_BLUE,
+    DARK_GREEN,
+    DARK_AQUA,
+    DARK_RED,
+    DARK_PURPLE,
+    GOLD,
+    GRAY,
+    DARK_GRAY,
+    BLUE,
+    GREEN,
+    AQUA,
+    RED,
+    LIGHT_PURPLE,
+    YELLOW,
+    WHITE,
+    OBFUSCATED,
+    BOLD,
+    STRIKETHROUGH,
+    UNDERLINE,
+    ITALIC,
+    RESET
+}
