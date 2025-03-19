90e4cd6ad5d04787e650980c20a6c1306aa1e068
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-03-19 17:46:06 +0200

    Registries

---
+++ net/minecraft/util/IRegistry.cs
@@ -0,0 +1,8 @@
+﻿namespace Minecraft1_8_9Port.net.minecraft.util;
+
+public interface IRegistry<TKey, TValue> : IEnumerable<TValue>
+{
+    TValue getObject(TKey name);
+
+    void putObject(TKey key, TValue value);
+}
---
+++ net/minecraft/util/RegistrySimple.cs
@@ -0,0 +1,60 @@
+﻿using Minecraft1_8_9Port.org.apache.logging.log4j;
+
+namespace Minecraft1_8_9Port.net.minecraft.util;
+
+public class RegistrySimple<TKey, TValue> : IRegistry<TKey, TValue>
+{
+    private static readonly Logger logger = LogManager.getLogger();
+    protected readonly Dictionary<TKey, TValue> registryObjects = createUnderlyingMap();
+    
+    protected static Dictionary<TKey, TValue> createUnderlyingMap()
+    {
+        return new Dictionary<TKey, TValue>();
+    }
+
+    public TValue getObject(TKey name)
+    {
+        return registryObjects.TryGetValue(name, out TValue value) ? value : default;
+    }
+
+    public void putObject(TKey key, TValue value)
+    {
+        if (key == null)
+        {
+            throw new ArgumentNullException(nameof(key));
+        }
+
+        if (value == null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        if (registryObjects.ContainsKey(key))
+        {
+            logger.debug($"Adding duplicate key '{key}' to registry");
+        }
+
+        registryObjects[key] = value;
+    }
+
+    public ICollection<TKey> getKeys()
+    {
+        return new List<TKey>(registryObjects.Keys);
+    }
+
+    public bool containsKey(TKey key)
+    {
+        return registryObjects.ContainsKey(key);
+    }
+
+    public IEnumerator<TValue> GetEnumerator()
+    {
+        return registryObjects.Values.GetEnumerator();
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    
+}
