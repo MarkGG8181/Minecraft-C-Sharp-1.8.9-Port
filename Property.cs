542aabe0f36590a3c06141e5edddbc08a3624f57
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-03-19 17:11:02 +0200

    realm porting

---
+++ Property.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Minecraft1_8_9Port;
+
+public class Property
+{
+    private readonly string name;
+    private readonly string value;
+    private readonly string signature;
+
+    public Property(string value, string name) : this(name, value, null) { }
+
+    public Property(string name, string value, string signature)
+    {
+        this.name = name;
+        this.value = value;
+        this.signature = signature;
+    }
+
+    public string getName()
+    {
+        return this.name;
+    }
+
+    public string getValue()
+    {
+        return this.value;
+    }
+
+    public string getSignature()
+    {
+        return this.signature;
+    }
+
+    public bool hasSignature()
+    {
+        return this.signature != null;
+    }
+
+    public bool isSignatureValid(RSA publicKey)
+    {
+        try
+        {
+            var sha1 = SHA1.Create();
+            var data = Encoding.UTF8.GetBytes(this.value);
+            var signatureBytes = Convert.FromBase64String(this.signature);
+            return publicKey.VerifyData(data, signatureBytes, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
+        }
+        catch (CryptographicException e)
+        {
+            Console.WriteLine(e);
+        }
+
+        return false;
+    }
+}
---
+++ PropertyMap.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Minecraft1_8_9Port;
+
+public class PropertyMap : Dictionary<string, List<Property>>
+{
+    public PropertyMap()
+    {
+    }
+
+    public new void Add(string key, Property value)
+    {
+        if (!ContainsKey(key))
+        {
+            this[key] = new List<Property>();
+        }
+
+        this[key].Add(value);
+    }
+
+    public class Serializer : JsonConverter<PropertyMap>
+    {
+        public override PropertyMap ReadJson(JsonReader reader, Type objectType, PropertyMap existingValue,
+            bool hasExistingValue, JsonSerializer serializer)
+        {
+            var result = new PropertyMap();
+            var jsonObject = JObject.Load(reader);
+
+            foreach (var property in jsonObject.Properties())
+            {
+                if (property.Value.Type == JTokenType.Array)
+                {
+                    foreach (var element in property.Value)
+                    {
+                        result.Add(property.Name, new Property(property.Name, element.ToString()));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public override void WriteJson(JsonWriter writer, PropertyMap value, JsonSerializer serializer)
+        {
+            var result = new JArray();
+
+            foreach (var propertyList in value.Values)
+            {
+                foreach (var property in propertyList)
+                {
+                    var jsonObject = new JObject
+                    {
+                        { "name", property.getName() },
+                        { "value", property.getValue() }
+                    };
+                    if (property.hasSignature())
+                    {
+                        jsonObject.Add("signature", property.getSignature());
+                    }
+
+                    result.Add(jsonObject);
+                }
+            }
+
+            result.WriteTo(writer);
+        }
+    }
+}
