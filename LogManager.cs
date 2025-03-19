57f25f4444a959a176c2b620cc5ab960d4c949f8
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-03-19 17:10:49 +0200

    Logger, LogManager

---
+++ LogManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Minecraft1_8_9Port;
+
+public class LogManager
+{
+    private static readonly Logger logger = new Logger();
+
+    public static Logger getLogger()
+    {
+        return logger;
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,17 @@
+﻿using Minecraft1_8_9Port.net.minecraft.client;
+
+namespace Minecraft1_8_9Port;
+
+public class Logger {
+    public void info(String message) {
+        Console.WriteLine(message);
+    }
+    
+    public void error(String message) {
+        Console.Error.WriteLine(message);
+    }
+    
+    public void warn(String message) {
+        Console.WriteLine(message);
+    }
+}
