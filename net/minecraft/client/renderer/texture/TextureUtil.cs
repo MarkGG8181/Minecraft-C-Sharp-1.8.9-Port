0c5d74303b42ece1935133b6758b9268afb48f29
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-03-19 18:39:50 +0200

    TextureUtil

---
+++ net/minecraft/client/renderer/texture/TextureUtil.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace Minecraft1_8_9Port.net.minecraft.client.renderer.texture;
+
+public class TextureUtil
+{
+    public static Bitmap readBufferedImage(Stream imageStream)
+    {
+        if (imageStream == null)
+        {
+            return null;
+        }
+
+        Bitmap bitmap = null;
+
+        try
+        {
+            bitmap = new Bitmap(imageStream);
+        }
+        finally
+        {
+            imageStream?.Dispose(); // Close the stream if it's not null
+        }
+
+        return bitmap;
+    }
+}
