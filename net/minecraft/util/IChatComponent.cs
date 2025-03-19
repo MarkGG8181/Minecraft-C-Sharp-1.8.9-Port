9eb31161eeda83439c018d745b42a34b16113aad
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-03-19 18:39:29 +0200

    Create IChatComponent.cs

---
+++ net/minecraft/util/IChatComponent.cs
@@ -0,0 +1,24 @@
+﻿namespace Minecraft1_8_9Port.net.minecraft.util;
+
+public interface IChatComponent //: Iterable<IChatComponent>
+{
+    IChatComponent setChatStyle(ChatStyle style);
+
+    ChatStyle getChatStyle();
+
+    IChatComponent appendText(String text);
+
+    IChatComponent appendSibling(IChatComponent component);
+
+    String getUnformattedTextForChat();
+
+    String getUnformattedText();
+
+    String getFormattedText();
+
+    List<IChatComponent> getSiblings();
+
+    IChatComponent createCopy();
+
+    //TODO: implement: Serializer(deserialize, serializeChatStyle, serialize, componentToJson, jsonToComponent)
+}
